3694e21821cce35e5b57fd9489471741a6b7b0ee
ZAF07 <zaffere07@gmail.com>
2024-06-17 09:03:42 +0800

    added remote repo

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from "../ui/dashboard/cards"
+
+// This is the / route
+export default function Dashboard() {
+    return (
+        <div className="flex min-h-screen flex-col p-6">
+            <p>Dashboard page</p>
+        </div>
+    )
+}
